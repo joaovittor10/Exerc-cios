3e017678630a47fd3f14f12527204ee14ac3820d
joaovittor10 <159137514+joaovittor10@users.noreply.github.com>
2024-03-03 19:34:25 -0300

    Create exercicio04.py

---
+++ exercicio04.py
@@ -0,0 +1,8 @@
+Exercicio
+4-Código que rode da seguinte maneira: Quando a palavra escola for encontrada na tela, digitar a palavra Yvone
+Resposta:
+
+palavra_na_tela = input("Digite a palavra na tela: ") 
+
+if palavra_na_tela == "escola":
+    print("Yvone")
