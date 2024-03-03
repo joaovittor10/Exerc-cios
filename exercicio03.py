e3b6f271b56dce70fe98760270fd788c612cdcac
joaovittor10 <159137514+joaovittor10@users.noreply.github.com>
2024-03-03 19:33:33 -0300

    Create exercicio03.py

---
+++ exercicio03.py
@@ -0,0 +1,8 @@
+Exercicio
+3-Código que use um item de vida em um personagem que estiver com a vida marcada em 1/10.
+Resposta:
+
+vida_do_personagem = 1/10
+
+if vida_do_personagem == 1/10:
+    usar_item_de_vida()
