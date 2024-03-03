3aadbc7e8878c566e05f10ea12017649315a2e04
joaovittor10 <159137514+joaovittor10@users.noreply.github.com>
2024-03-03 19:31:30 -0300

    Create exercicio01.py

---
+++ exercicio01.py
@@ -0,0 +1,8 @@
+Exercicios
+1-Código que faça a rotina de esvaziar um inventário de um jogo
+Resposta:
+
+inventario = ["item1", "item2", "item3", ...]
+while inventario:
+    item = inventario.pop()
+   print("Esvaziando:", item)
